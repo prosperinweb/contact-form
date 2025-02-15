ea3936c5bd6dae82205032b03961eb315d623926
prosperinweb <ofentsenglazi@gmail.com>
2025-02-15 02:48:01 +0200

    Enhance input validation with error messages and length restrictions

--- src/components/views/Input.tsx
+++ src/components/views/Input.tsx
@@ -7,12 +7,17 @@ const Input = ({ type, name, label }: InputProps) => {
         {label} <span className="text-green-600">*</span>
       </label>
       <input
-        className="border border-green-600 rounded-sm px-4 py-2 focus:outline-none focus:ring-1 focus:ring-green-600 input:user-valid:border-red-600 transition-all duration-200"
+        className="border border-green-600 rounded-sm px-4 py-2 focus:outline-none focus:ring-1 focus:ring-green-600 [&:user-invalid]:border-red-600 [&:user-invalid]:ring-red-500 transition-all duration-200"
         type={type}
         name={name}
+        maxLength={320} //max length as per RFC 5321
+        minLength={2}
         id={name}
         required
       />
+      <p className="error-message opacity-0">
+        Please enter a valid {label.toLowerCase()}
+      </p>
     </div>
   );
 };
