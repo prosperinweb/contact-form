8d021b097961830d82c6eb10be9f6ff87c1c41b5
prosperinweb <ofentsenglazi@gmail.com>
2025-02-19 22:42:25 +0200

    Fix header formatting by removing extra space in title

--- src/components/containers/Header.tsx
+++ src/components/containers/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="">
-      <h1 className="text-2xl font-700 text-center"> Contact Form</h1>
+      <h1 className="text-2xl font-700 text-center">Contact Form</h1>
     </header>
   );
 };
